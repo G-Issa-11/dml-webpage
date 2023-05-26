c1a8be3eebeb28a1741596a7f7e6208233e8a7ca
G-issa-11 <ghada.issa314@gmail.com>
2023-05-26 15:57:51 +0300

    final design

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ function carousel() {
   x[myIndex-1].style.display = "block";  
   setTimeout(carousel, 2000); // Change image every 2 seconds
 }
+
+var vc = document.getElementById('video-container');
+
+
+vc.addEventListener("click", function(e) {
+  e.preventDefault();
+  document.getElementById("overlay").style.display = "block";
+  console.log("hello")
+});
+
+
+
+var closeButtons = document.getElementsByClassName("closeBtn");
+    for (var i = 0; i < closeButtons.length; i++) {
+      closeButtons[i].addEventListener("click", function() {
+        document.getElementById("overlay").style.display = "none";
+      });
+    }
